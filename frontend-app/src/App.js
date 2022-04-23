6c743fea3b02a84788dac79be46fb9d283c26e8c
Israel Arellano <israel.arellano.gdz@gmail.com>
2022-04-22 19:58:47 -0500

    install react

---
+++ frontend-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageUploader from './components/ImageUploader';
+
+function App() {
+  return (
+    <div className="App">
+		<ImageUploader></ImageUploader>
+    </div>
+  );
+}
+
+export default App;
